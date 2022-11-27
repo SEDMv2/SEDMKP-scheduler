ed1d649d1151b17e7965fa97ebe270270d3ed9ad
Yashvi Sharma <yashuvatsas@gmail.com>
2022-11-26 22:44:36 -0700

    Small cpp script to test calling of get_observation

---
+++ testc.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+int error = 0;
+std::stringstream cmd;
+cmd << "/media/yashvi/Data/Work/sedmv2/" << "get_observation";
+error = system(cmd.str().c_str());
+cout << "error code " << error;
+if(WEXITSTATUS(error) == 0){
+	cout << "here";
+};
+// return 0;
+}
+// printf(WEXITSTATUS(error));
