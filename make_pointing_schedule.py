0a86fb9a9e8f10681bd41fe99deca085c41ea44a
Yashvi Sharma <yashuvatsas@gmail.com>
2023-05-03 17:37:26 -0700

    Added feature to use schedule file instead of fritz

---
+++ make_pointing_schedule.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+ordered_altaz = []
+azimuths = np.arange(0,351,10)
+count = 0
+for az in azimuths:
+    for alt in np.arange(30,81,10):
+        ordered_altaz.append(['point','-99',f'point_{count}','-99',az,alt,'2000',120.0,'cl'])
+        count = count+1
+
+df = pd.DataFrame(columns=["requester", "group_id", "object_id", "request_id", "ra", "dec", "epoch",
+                    "exposure_time","observation_choice"], data=ordered_altaz)
+df.to_csv('fixed_schedule.csv',index=False)
