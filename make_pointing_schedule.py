4d5ee8f7d534fbc621c42bda0bf6ee94a835ecd1
Yashvi Sharma <yashuvatsas@gmail.com>
2023-05-03 19:12:27 -0700

    updated exptime from 120s to 60s

--- make_pointing_schedule.py
+++ make_pointing_schedule.py
@@ -6,7 +6,7 @@ azimuths = np.arange(0,351,10)
 count = 0
 for az in azimuths:
     for alt in np.arange(30,81,10):
-        ordered_altaz.append(['point','-99',f'point_{count}','-99',az,alt,'2000',120.0,'cl'])
+        ordered_altaz.append(['point','-99',f'point_{count}','-99',az,alt,'2000',60.0,'cl'])
         count = count+1
 
 df = pd.DataFrame(columns=["requester", "group_id", "object_id", "request_id", "az", "alt", "epoch",
