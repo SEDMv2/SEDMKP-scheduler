7441ef3569552efbad0d919405eca21ffb0c1a89
Yashvi Sharma <yashuvatsas@gmail.com>
2023-05-11 07:25:04 +0200

    fixed program id and request id for pointing file

--- make_pointing_schedule.py
+++ make_pointing_schedule.py
@@ -6,7 +6,7 @@ azimuths = np.arange(0,351,10)
 count = 0
 for az in azimuths:
     for alt in np.arange(30,81,10):
-        ordered_altaz.append(['point','-99',f'point_{count}','-99',az,alt,'2000',70.0,'cl'])
+        ordered_altaz.append(['point','999',f'point_{count}',count,az,alt,'2000',70.0,'cl'])
         count = count+1
 
 df = pd.DataFrame(columns=["requester", "group_id", "object_id", "request_id", "az", "alt", "epoch",
